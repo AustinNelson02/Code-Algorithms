90ee67caa096b9f71eda03cf6ccabbbe20e10562
AustinNelson02 <107258966+AustinNelson02@users.noreply.github.com>
2024-01-23 10:13:01 -0600

    Add files via upload

---
+++ Matlab Code/Direct Methods/Cholesky.m
@@ -0,0 +1,20 @@
+function [A] = Cholesky(A)
+    [n,n] = size(A);
+
+    for j = 1:n
+        A(j,j) = sqrt(A(j,j));
+        for i = j+1:n
+            A(i,j) = A(i,j)/A(j,j);
+        end
+        for k = j+1:n
+            for i = k:n
+                A(i,k) = A(i,k) - A(i,j)*A(k,j)
+            end
+        end
+    end
+
+    for i = 1:n-1
+        for j = i+1:n
+            A(i,j) = 0;
+        end
+    end
