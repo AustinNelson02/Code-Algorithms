b0b0737497e3fe8a910e274b4cf080bc51fdca28
AustinNelson02 <107258966+AustinNelson02@users.noreply.github.com>
2024-04-17 21:42:30 -0500

    Update Projections.py

--- Python Code/Serial Algorithms/Projections.py
+++ Python Code/Serial Algorithms/Projections.py
@@ -5,7 +5,7 @@ from LinearSolvers import *
 
 # Solves the minimum norm problem
 def min_norm(C,d):
-    [Q,R,rank] = QR(np.transpose(C),.0001)
+    [Q,R,rank] = QR(np.transpose(C),.0000001)
     w = forwardSub(np.transpose(R),d)
     x = backwardSub(R,w)
 
@@ -18,6 +18,9 @@ def projection(v,C,d):
 
     return x + v
 
+# Finds the projection of v onto the half plane of Cz = d depending on which side you choose.
+# Giving a value of 0 will project onto Cz <= d.
+# Giving a value of 1 will project onto Cz >= d.
 @dispatch(np.ndarray,np.ndarray,np.ndarray,int)
 def projection(v,C,d,s):
     m = len(C)
@@ -37,6 +40,7 @@ def projection(v,C,d,s):
         x = projection(v,C,d)
     return x
 
+# Finds the projection of v onto the ball of radius r centered at the origin.
 @dispatch(np.ndarray,float)
 def projection(v,r):
     norm = np.linalg.norm(v)
@@ -45,40 +49,44 @@ def projection(v,r):
     else:
         return v*(r/norm)
 
+# Performs the Dykstra Algorithm to find the projection of v onto multiple convex sets.
+# The available convex sets are the hyperplane, halfspace/quadrant, and ball of radius r at the origin.
+# The parameters of the function are as follows.
+#
+# v : The vector we would like to project.
+# LP: A list of convex sets to project onto. The format accepts a list of lists, where each element is a list
+#     of convex sets.
+# maxk: Maximum number of iterations.
+# tol: Tolerance for the algorithm.
 def dykstra(v,LP,maxk,tol):
 
-    # We first need to know what the size of v is to make the auxiliary vectors.
+    # Find the length of all of the vectors.
     n = len(v)
 
-    # We need to know how many vectors to create based on how many convex sets we look at.
+    # Find how many convex sets are given.
     m = len(LP)
 
-    # We can create the initial vector and what to update by creating a matrix of vectors
-    # whose rows is the size of v and columns is the size of how many projections we have.
+    # Initialize a matrix holding the vectors that will be projected on in the algorithm.
     X = np.zeros((n,m))
 
-    # We start with projecting the original vector, so make a copy of it into an arbitrary vector x_0
+    # Initialize the auxiliary vector and projection vector. x_0 will hold the final projection in the end.
+    # So, we will return that.
     x_0 = copy.deepcopy(v)
-    print(x_0)
     diff = np.zeros((n,1))
     diff_norm = 10
     p = np.zeros((n,1))
-
     k = 0
 
-    # We need to initialize what the elements of X is by first performing the projection on every
-    # vector with respect to v.
-    # We can just traverse the actual elements of the list for this.
+    # Performance of the Dykstra Algorithm for a given list of convex sets.
     while diff_norm > tol and k < maxk:
         for i in range(len(LP)):
-            print(x_0 + X[:,[i]])
             # Compute the projection.
             p = projection(x_0 + X[:,[i]],*LP[i])
 
             # Compute the ith vector of X based on the computed projection.
             X[:,[i]] = X[:,[i]] + x_0 - p
 
-            # updating x_0 to be the computed projection p.
+            # Updating x_0 to be the computed projection p.
             x_0 = copy.deepcopy(p)
 
             # Finding the difference of the computed projections so we can find an initial error.
@@ -87,10 +95,10 @@ def dykstra(v,LP,maxk,tol):
             else:
                 diff = diff - p
 
+        # Update conditions on the loop to determine if we converged.
         diff_norm = np.linalg.norm(diff)
         k = k + 1
 
-        return X[:,[-1]]
-
-            
+    # Return the final projection of the algorithm.
+    return x_0
 
